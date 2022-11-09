a21f15123a51fd52d373d363398de986c254e5e1
Martin <sales@sopherapps.com>
2022-11-09 05:26:07 +0300

    Enhance benchmark tests to be more accurate

--- benches/scdb.rs
+++ benches/scdb.rs
@@ -1,26 +1,22 @@
 use std::iter::{IntoIterator, Iterator};
 use std::string::ToString;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 
 use scdb::Store;
 
 const STORE_PATH: &str = "testdb";
 
 // Setting
-fn setting_benchmark(c: &mut Criterion) {
+fn setting_without_ttl_benchmark(c: &mut Criterion) {
     let mut store = Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
     store.clear().expect("clear store");
     let records = get_records();
     for (k, v) in &records {
         c.bench_function(
-            &format!(
-                "set {} {}",
-                String::from_utf8(k.clone()).unwrap(),
-                String::from_utf8(v.clone()).unwrap()
-            ),
+            &format!("set(no ttl): '{}'", String::from_utf8(k.clone()).unwrap(),),
             |b| {
-                b.iter(|| {
+                b.iter_with_large_drop(|| {
                     store.set(
                         black_box(&k.clone()),
                         black_box(&v.clone()),
@@ -32,8 +28,25 @@ fn setting_benchmark(c: &mut Criterion) {
     }
 }
 
+fn setting_with_ttl_benchmark(c: &mut Criterion) {
+    let mut store = Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
+    store.clear().expect("clear store");
+    let ttl = Some(3_600u64);
+    let records = get_records();
+    for (k, v) in &records {
+        c.bench_function(
+            &format!("set(ttl): '{}'", String::from_utf8(k.clone()).unwrap(),),
+            |b| {
+                b.iter_with_large_drop(|| {
+                    store.set(black_box(&k.clone()), black_box(&v.clone()), black_box(ttl))
+                })
+            },
+        );
+    }
+}
+
 // Updating
-fn updating_benchmark(c: &mut Criterion) {
+fn updating_without_ttl_benchmark(c: &mut Criterion) {
     let mut store = Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
     store.clear().expect("clear store");
     let records = get_records();
@@ -44,58 +57,140 @@ fn updating_benchmark(c: &mut Criterion) {
     for (k, v) in &updates {
         c.bench_function(
             &format!(
-                "update {} to {}",
+                "update(no ttl): '{}'",
                 String::from_utf8(k.clone()).unwrap(),
-                String::from_utf8(v.clone()).unwrap()
             ),
-            |b| b.iter(|| store.set(black_box(k), black_box(v), black_box(None))),
+            |b| b.iter_with_large_drop(|| store.set(black_box(k), black_box(v), black_box(None))),
+        );
+    }
+}
+
+fn updating_with_ttl_benchmark(c: &mut Criterion) {
+    let mut store = Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
+    store.clear().expect("clear store");
+    let records = get_records();
+    let updates = get_updates();
+    let ttl = Some(3_600u64);
+    for (k, v) in &records {
+        store.set(k, v, ttl).expect(&format!("set {:?}", k));
+    }
+    for (k, v) in &updates {
+        c.bench_function(
+            &format!("update(ttl): '{}'", String::from_utf8(k.clone()).unwrap(),),
+            |b| b.iter_with_large_drop(|| store.set(black_box(k), black_box(v), black_box(ttl))),
         );
     }
 }
 
 // Getting
-fn getting_benchmark(c: &mut Criterion) {
+fn getting_without_ttl_benchmark(c: &mut Criterion) {
     let mut store = Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
     store.clear().expect("clear store");
     let records = get_records();
     for (k, v) in &records {
         store.set(k, v, None).expect(&format!("set {:?}", k));
     }
+
     for (k, _) in &records {
         c.bench_function(
-            &format!("get {}", String::from_utf8(k.clone()).unwrap()),
-            |b| b.iter(|| store.get(black_box(k))),
+            &format!("get(no ttl): '{}'", String::from_utf8(k.clone()).unwrap()),
+            |b| b.iter_with_large_drop(|| store.get(black_box(k))),
+        );
+    }
+}
+
+fn getting_with_ttl_benchmark(c: &mut Criterion) {
+    let mut store = Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
+    store.clear().expect("clear store");
+    let records = get_records();
+    let ttl = Some(3_600u64);
+
+    for (k, v) in &records {
+        store.set(k, v, ttl).expect(&format!("set {:?}", k));
+    }
+    for (k, _) in &records {
+        c.bench_function(
+            &format!("get(with ttl): '{}'", String::from_utf8(k.clone()).unwrap()),
+            |b| b.iter_with_large_drop(|| store.get(black_box(k))),
         );
     }
 }
 
 // Deleting
-fn deleting_benchmark(c: &mut Criterion) {
+fn deleting_without_ttl_benchmark(c: &mut Criterion) {
+    let records = get_records();
     let mut store = Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
     store.clear().expect("clear store");
-    let records = get_records();
     for (k, v) in &records {
         store.set(k, v, None).expect(&format!("set {:?}", k));
     }
 
     for (k, _) in &records {
         c.bench_function(
-            &format!("delete {}", String::from_utf8(k.clone()).unwrap()),
+            &format!(
+                "delete(no ttl): '{}'",
+                String::from_utf8(k.clone()).unwrap()
+            ),
+            |b| b.iter(|| store.delete(black_box(k))),
+        );
+    }
+}
+
+fn deleting_with_ttl_benchmark(c: &mut Criterion) {
+    let records = get_records();
+    let ttl = Some(3_600u64);
+    let mut store = Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
+    store.clear().expect("clear store");
+    for (k, v) in &records {
+        store.set(k, v, ttl).expect(&format!("set {:?}", k));
+    }
+
+    for (k, _) in &records {
+        c.bench_function(
+            &format!("delete(ttl): '{}'", String::from_utf8(k.clone()).unwrap()),
             |b| b.iter(|| store.delete(black_box(k))),
         );
     }
 }
 
 // Clearing
-fn clearing_benchmark(c: &mut Criterion) {
-    let mut store = Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
-    store.clear().expect("clear store");
-    let records = get_records();
-    for (k, v) in &records {
-        store.set(k, v, None).expect(&format!("set {:?}", k));
-    }
+fn clearing_without_ttl_benchmark(c: &mut Criterion) {
+    c.bench_function("clear(no ttl)", |b| {
+        b.iter_batched(
+            || {
+                let mut store =
+                    Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
+                store.clear().expect("clear store");
+                let records = get_records();
+                for (k, v) in &records {
+                    store.set(k, v, None).expect(&format!("set {:?}", k));
+                }
+                store
+            },
+            |mut store| store.clear(),
+            BatchSize::PerIteration,
+        )
+    });
+}
 
-    c.bench_function("clear", |b| b.iter(|| store.clear()));
+fn clearing_with_ttl_benchmark(c: &mut Criterion) {
+    c.bench_function("clear(ttl)", |b| {
+        b.iter_batched(
+            || {
+                let mut store =
+                    Store::new(STORE_PATH, None, None, None, Some(0)).expect("create store");
+                store.clear().expect("clear store");
+                let records = get_records();
+                let ttl = Some(3_600u64);
+                for (k, v) in &records {
+                    store.set(k, v, ttl).expect(&format!("set {:?}", k));
+                }
+                store
+            },
+            |mut store| store.clear(),
+            BatchSize::PerIteration,
+        )
+    });
 }
 
 // Compacting
@@ -148,11 +243,16 @@ fn get_updates() -> Vec<(Vec<u8>, Vec<u8>)> {
 
 criterion_group!(
     benches,
-    setting_benchmark,
-    updating_benchmark,
-    getting_benchmark,
-    deleting_benchmark,
-    clearing_benchmark,
+    setting_without_ttl_benchmark,
+    setting_with_ttl_benchmark,
+    updating_without_ttl_benchmark,
+    updating_with_ttl_benchmark,
+    getting_without_ttl_benchmark,
+    getting_with_ttl_benchmark,
+    deleting_without_ttl_benchmark,
+    deleting_with_ttl_benchmark,
+    clearing_without_ttl_benchmark,
+    clearing_with_ttl_benchmark,
     compacting_benchmark,
 );
 criterion_main!(benches);
